15c6bf98a755c5be44784bc5998f8ffb821fcd34
Navalhurpade <68598490+Navalhurpade@users.noreply.github.com>
2020-12-13 01:59:51 +0530

    uncomented all the code in NoteData

--- src/components/NotesData.jsx
+++ src/components/NotesData.jsx
@@ -1,33 +1,33 @@
-// import { useState } from "react";
+import { useState } from "react";
 
-// function notes(){
+function notes(){
   
 
-//     this.addNote = newNote => {
-//   // setNote((prevValue) => [...prevValue, newNote]);
-//   // event.preventDefault();
-// console.log("got in function ");
+    this.addNote = newNote => {
+  // setNote((prevValue) => [...prevValue, newNote]);
+  // event.preventDefault();
+console.log("got in function ");
 
-// }
+}
 
-//    this.deleteNote = id=>{
-//   notes.map((noteobj) => {
-//     if (noteobj.id === id) {
-//       const index = notes.indexOf(noteobj);
-//       notes.splice(index, 1);
-//       console.log(noteobj + " \nis Deleted !");
-//     } else {
-//       console.log("Cannot find Note !");
-//     }
-//   });
-// }
+   this.deleteNote = id=>{
+  notes.map((noteobj) => {
+    if (noteobj.id === id) {
+      const index = notes.indexOf(noteobj);
+      notes.splice(index, 1);
+      console.log(noteobj + " \nis Deleted !");
+    } else {
+      console.log("Cannot find Note !");
+    }
+  });
+}
 
-// return note
-// }
+return note
+}
 
-// // const fun = new notes()
+// const fun = new notes()
 
-// // console.log(fun.addNote());
+// console.log(fun.addNote());
 
-// export default notes;
-// // export { addNote, deleteNote };
+export default notes;
+// export { addNote, deleteNote };
